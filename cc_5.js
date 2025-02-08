72d16ab804edc4d4cb7deda0caad9095605e2bbc
Dylan <safirodylan@gmail.com>
2025-02-08 17:33:27 -0500

    Task 1 - Customer Profile

---
+++ cc_5.js
@@ -0,0 +1,8 @@
+// Task 1: Customer Profile
+let customer = {name: "John Doe", 
+    age: 35, 
+    email: "johndoe@email.com"
+}; // Declares customer profile as an object
+console.log(`Customer Name: ${customer.name}`); // Outputs customer name
+console.log(`Customer Age: ${customer.age}`); // Outputs customer age
+console.log(`Customer Email: ${customer.email}`); // Outputs customer email
