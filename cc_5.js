d34b535cf092715981059cabd84983eef41a2247
Dylan <safirodylan@gmail.com>
2025-02-08 22:40:08 -0500

    Task 2 - Order Details

--- cc_5.js
+++ cc_5.js
@@ -6,3 +6,14 @@ let customer = {name: "John Doe",
 console.log(`Customer Name: ${customer.name}`); // Outputs customer name
 console.log(`Customer Age: ${customer.age}`); // Outputs customer age
 console.log(`Customer Email: ${customer.email}`); // Outputs customer email
+
+// Task 2: Order Details
+let order = {
+    orderId: 12345,
+    totalAmount: 150,
+    status: "Processing",
+    displayOrder () { // Declares displayOrder as a function inside object
+        return console.log(`Order ID: ${this.orderId} | Total Amount: $${this.totalAmount} | Status: ${this.status}`) // Outputs order details 
+    }
+}; // Declares order details as an object
+order.displayOrder(); // Returns displayOrder and outputs function
